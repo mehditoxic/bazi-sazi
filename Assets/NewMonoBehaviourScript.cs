b34d1cb859334f8fb07873202aab09bbbe19773b
mahdi balaee <mahdibalaee@gmail.com>
2025-04-27 08:38:34 +0330

    game

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+   
+        if (moveInput > 0)
+            transform.localScale = new Vector3(-1, 1, 1); 
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+        if (Mathf.Abs(moveInput) > 0.5f)
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isIdle", false);
+        }
+        else
+        {
+            animator.SetBool("isIdle", true);
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+}
---
+++ Assets/Tasty_Characters - Castle Pack/Assets/Prefabs/fire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ParticleShooter : MonoBehaviour
+{
+    public GameObject fireballPrefab; // Prefab گلوله آتیش
+    public Transform firePoint;       // نقطه‌ی شروع شلیک
+    public float fireballSpeed = 10f;  // سرعت گلوله
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // وقتی Space فشار داده شد
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject fireball = Instantiate(fireballPrefab, firePoint.position, Quaternion.identity);
+        Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = transform.right * fireballSpeed; // حرکت به جلو
+    }
+}
